e9e8b62ee6464f3b72b0dee8bdcb0875b81f5d02
derek.langenkam <derek.langenkam@SEA-8E413E>
2016-08-22 16:19:29 -0700

    Add project files.

---
+++ Password/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>	
+
+int main()
+{
+	printf("Hello World!");
+
+	//comments lololololol
+	int lol;
+
+	lol = 235;
+
+	int kek = lol /8.0 + 7;
+
+	printf(" %d %d", lol, kek);
+
+	getchar();
+}
